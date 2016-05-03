1fafaf12e19da9ef355eb9ae97edbd23478efa2e
Jobronies <spencerasproul@gmail.com>
2016-05-03 10:29:30 -0700

    Initial Commit

---
+++ inc/footer.php
@@ -0,0 +1,3 @@
+<footer>
+  <p>Copyright (c) 2016 Copyright Fourth Moon Design | All Rights Reserved</p>
+</footer>
---
+++ inc/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <!-- Stylesheet and Framework -->
+  <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <!-- Slideshow -->
+  <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+  <!-- Link to Google Resources -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- Fonts -->
+  <link href='https://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto:500' rel='stylesheet' type='text/css'>
+  <!-- Title and Favicon -->
+  <title>Fourth Moon Design</title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <!-- Meta and Credits -->
+  <meta name="description" content="Fourth Moon Design">
+  <meta name="keywords" content="HTML,CSS,Javascript,PHP">
+  <meta name="author" content="Spencer Sproul">
+</head>
+
+<body>
+
+  <!-- Header -->
+
+<header class="w3-top">
+
+  <!-- Announcement Bar -->
+
+  <div id='fbar'><p>Fourth Moon</p></div>
+
+  <div id='abar'>
+    <p>PAYMENT NOT YET OPERATIONAL, CONTACT ORDER@FOURTHMOONDESIGN.COM TO PLACE AN ORDER<a id="cart" href="#"><i class="material-icons">shopping_cart</i></a></p>
+  </div>
+
+  <!-- Navigation -->
+
+  <ul class="navbar">
+    <li id="logo"><a href="#"><img class="logo" src="img/navlogo-invert.png"></a></li>
+    <li><a href="shop.html">Shop</a></li>
+    <li><a href="about.html">About</a></li>
+    <li id="search"></li>
+  </ul>
+</header>
